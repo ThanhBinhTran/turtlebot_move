7674a59c7a9e9e5fe2ea5b07abedcc2eb0a2b95d
ThanhBinhTran <thanhbinh.hcmut@gmail.com>
2022-03-08 16:05:57 +0700

    update robot_move_lib functions

--- src/robot_move_lib.h
+++ src/robot_move_lib.h
@@ -15,25 +15,43 @@ namespace robot_base_driver
         ros::Publisher cmd_vel_pub;     // cmd_vel publisher
         geometry_msgs::Twist cmd_msg;   // cmd_vel geometry message
         
-        // default constructor and deconstructor
+        /*
+         * constructor and deconstructor
+         */
+        
         robot_move(ros::NodeHandle nh, std::string topic_name);
         ~robot_move();
 
-        /* ROS and real robot functionality handler */
+        /*
+         * ROS and real robot functionality handler 
+         */
+
         void do_publish(uint run_times, float topic_rate); // publish message to topic
         bool check_velocity_limitation(float linear_vel_x, float linear_vel_y, float linear_vel_z,
                 float angular_vel_x, float angular_vel_y, float angular_vel_z); // return true if velocity is in limited vel ranges
 
-        /* message preparation */
+        /*
+         * message preparation 
+         */
+
         void reset_cmd_msg();   // reset cmd_vel geometry message
         void set_linear(float numx, float numy, float numz); // set linear for cmd_vel geometry message
         void set_angular(float numx, float numy, float numz); // set angular for cmd_vel geometry message
 
-        /* movement functions */
+        /* 
+         * movement functions 
+         */
+        
         void stop_move(); // stop moving
-        void go_straight(float distance_in_m, float time_period);  // go straight (meter) in time_period second; distance_in_m = (+) forward, (-) backward
-        void rotate(float radian, float time_period);  // rotate (radian) in time_period second; radian = (+) anticlockwise, (-) clockwise. 
-        void arc(float theta, float radius, float time_period); // robot follows arc defined by theta and radian in time_period second.
+        
+        /* go straight (meter) in time_period second; distance_in_m = (+) forward, (-) backward */
+        void go_straight(float distance_in_m, float time_period); 
+        
+        /* rotate (radian) in time_period second; radian = (+) anticlockwise, (-) clockwise. */
+        void rotate(float radian, float time_period);  
+        
+        /* robot follows arc defined by theta and radian in time_period second.*/
+        void arc(float theta, float radius, float time_period); 
     };
 
 }    
